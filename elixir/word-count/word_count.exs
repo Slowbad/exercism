7dedfe20fb2e00fe550b8b5d0d837849210e5ab4
Steven Martin <steven@stevenmartin.io>
2015-12-19 14:50:44 -0500

    Elixir/word_count clarified variable and function names

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -7,13 +7,13 @@ defmodule Words do
   @spec count(String.t) :: map()
   def count(sentence) do
     String.split(sentence)
-    |> List.foldl(%{}, &do_things/2)
+    |> List.foldl(%{}, &count_word_occurances/2)
   end
 
-  def do_things(ele, acc) do
-    case acc[ele] do
-      nil -> Map.put(acc, ele, 1)
-      _   -> Map.put(acc, ele, acc[ele] + 1)
+  def count_word_occurances(word, word_counts) do
+    case word_counts[word] do
+      nil -> Map.put(word_counts, word, 1)
+      _   -> Map.put(word_counts, word, word_counts[word] + 1)
     end
   end
 end
