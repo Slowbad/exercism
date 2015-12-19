be3100219959aa26a837320f3898282aa8ef0cd8
Steven Martin <steven@stevenmartin.io>
2015-12-19 12:47:21 -0500

    Elixir/bob extracted method to clarify intent

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -2,7 +2,7 @@ defmodule Bob do
   def hey(input) do
     cond do
       silence?(input) -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
+      question?(input) -> "Sure."
       input == String.upcase(input) and String.match?(input, ~r/\p{L}/) -> "Whoa, chill out!"
       true -> "Whatever."
     end
@@ -10,5 +10,9 @@ defmodule Bob do
 
   defp silence?(input) do
     String.strip(input) == ""
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
   end
 end
