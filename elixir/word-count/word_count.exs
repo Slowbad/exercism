cc63296078750bb42c35b12531994ea539b827ad
Steven Martin <steven@stevenmartin.io>
2015-12-19 18:39:40 -0500

    Elixir/word_count moved secondary responisibility from counting function to another set of functions.

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -7,14 +7,25 @@ defmodule Words do
   @spec count(String.t) :: map()
   def count(sentence) do
     Regex.scan(~r/(\p{L}+(-\p{L}+)?|\d+)/, sentence)
+    |> extract_scan_matches
     |> List.foldl(%{}, &count_word_occurances/2)
   end
 
-  def count_word_occurances(word_match, word_counts) do
-    [word | _] = word_match
+  defp count_word_occurances(word, word_counts) do
     case word_counts[word] do
       nil -> Map.put(word_counts, word, 1)
       _   -> Map.put(word_counts, word, word_counts[word] + 1)
     end
   end
+
+  @doc """
+  Extract a list of regex matches from Regex.scan, ignoring capture groups.
+  """
+  defp extract_scan_matches(scan_list), do: extract_scan_matches(scan_list, [])
+  defp extract_scan_matches([], matches), do: matches
+  defp extract_scan_matches([head | tail], matches) do
+    [match | _] = head
+    extract_scan_matches(tail, [match | matches])
+  end
+
 end
