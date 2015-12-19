bf8f1075d542517c6198fc00bb1107314b63bb55
Steven Martin <steven@stevenmartin.io>
2015-12-19 14:42:46 -0500

    Elixir/word_count counts a single occurance of all words in the sentence

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,6 +6,11 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
-    %{"word" => 1}
+    String.split(sentence)
+    |> List.foldl(%{}, &do_things/2)
+  end
+
+  def do_things(ele, acc) do
+    Map.put(acc, ele, 1)
   end
 end
